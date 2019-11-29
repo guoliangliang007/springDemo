a45adac711ef3f813ef82d8c059da7d7102b7ef2
guoliangliang007 <1136941477@qq.com>
2019-11-29 11:24:10 +0800

    springboot+springSecurity+layui

--- src/main/java/com/example/demo/redis/RedisConfig.java
+++ src/main/java/com/example/demo/redis/RedisConfig.java
@@ -5,13 +5,14 @@ import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.*;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
@@ -20,9 +21,10 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 import java.time.Duration;
 
 @Configuration
+@EnableCaching //开启注解
 public class RedisConfig extends CachingConfigurerSupport {
 
-    @Bean(name = "redisTemplate")
+    @Bean//(name = "redisTemplate")
     public RedisTemplate<Object,Object> redisTemplate(RedisConnectionFactory redisConnectionFactory){
         RedisTemplate<Object,Object> template = new RedisTemplate<>();
         template.setConnectionFactory(redisConnectionFactory);
@@ -64,4 +66,5 @@ public class RedisConfig extends CachingConfigurerSupport {
     //public User findById(@Param("id") Integer id);
     //上述代码的作用，是在调用findById方法时优先查询redis中的缓存数据。
     //如果redis对应缓存不存在，则请求mysql查询数据，并将数据缓存到redis中，设置缓存的过期时间为30秒
+
 }
